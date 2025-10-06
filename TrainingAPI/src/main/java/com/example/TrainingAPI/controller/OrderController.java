fc4721c57fcbedde7dff4a496edb1c20a7d9df60
An <nguyenlebinhank63@gmail.com>
2025-10-06 17:00:43 +0700

    Add OrderService

---
+++ TrainingAPI/src/main/java/com/example/TrainingAPI/controller/OrderController.java
@@ -0,0 +1,38 @@
+package com.example.TrainingAPI.controller;
+
+import com.example.TrainingAPI.payload.OrderDTO;
+import com.example.TrainingAPI.payload.OrderRequestDTO;
+import com.example.TrainingAPI.service.OrderService;
+import com.example.TrainingAPI.util.AuthUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private AuthUtil authUtil;
+
+    @PostMapping("/order/user/payments/{paymentMethod}")
+    public ResponseEntity<OrderDTO> orderProducts(@PathVariable String paymentMethod,
+                                                  @RequestBody OrderRequestDTO orderRequestDTO){
+        String emailId = authUtil.loggedInEmail();
+        OrderDTO orderDTO=orderService.placeOrder(
+                emailId,
+                orderRequestDTO.getAddressId(),
+                paymentMethod,
+                orderRequestDTO.getPgName(),
+                orderRequestDTO.getPgPaymentId(),
+                orderRequestDTO.getPgStatus(),
+                orderRequestDTO.getPgResponseMessage()
+        );
+        return new ResponseEntity<>(orderDTO, HttpStatus.CREATED);
+
+    }
+}
---
+++ TrainingAPI/src/main/java/com/example/TrainingAPI/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.example.TrainingAPI.service;
+
+import com.example.TrainingAPI.payload.OrderDTO;
+
+public interface OrderService {
+    OrderDTO placeOrder(String emailId, Long addressId, String paymentMethod, String pgName, String pgPaymentId, String pgStatus, String pgResponseMessage);
+}
